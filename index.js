b2f70a7108b5c18c3d6579bd128fad2adde2e4f7
Damian <Your Github Email Address>
2021-09-29 18:48:59 +0000

    we tried question 2

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+//1
 function OneHundredToFive() {
     for (let i = 100; i >= 5; i-=1) {
         console.log(i)
     }
 }
 
-OneHundredToFive()
-//testing
+//OneHundredToFive()
+
+//2
+/*
+function absoluteValue(num){
+    if(num < 0){
+ return(num * -1)
+    } 
+    if (num === null ){
+    return null
+    }
+}
+console.log(absoluteValue(7.33))
+*/
